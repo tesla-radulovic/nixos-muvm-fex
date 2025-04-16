a9b836b4f5de8cdde993e18b1c427167d5087e02
Nikodem Rabuliński <nikodem@rabulinski.com>
2025-04-16 13:34:58 +0200

    Make overlay code nicer

--- overlay.nix
+++ overlay.nix
@@ -1,24 +1,30 @@
 { nixos-apple-silicon-overlay }:
-final: prev:
 let
-  overlayed = nixos-apple-silicon-overlay final prev;
+  # This overlay assumes all previous overlays have been applied
+  overlay = final: prev: {
+    virglrenderer = prev.virglrenderer.overrideAttrs (old: {
+      src = final.fetchurl {
+        url = "https://gitlab.freedesktop.org/asahi/virglrenderer/-/archive/asahi-20241205.2/virglrenderer-asahi-20241205.2.tar.bz2";
+        hash = "sha256-mESFaB//RThS5Uts8dCRExfxT5DQ+QQgTDWBoQppU7U=";
+      };
+      mesonFlags = old.mesonFlags ++ [ (final.lib.mesonOption "drm-renderers" "asahi-experimental") ];
+    });
+    libkrun = final.callPackage ./libkrun.nix { };
+    mesa-asahi-edge = final.callPackage ./mesa.nix { inherit (prev) mesa-asahi-edge; };
+    muvm = final.callPackage ./muvm.nix { mesa-x86_64-linux = final.pkgsCross.gnu64.mesa-asahi-edge; };
+    fex = final.callPackage ./fex.nix { };
+    fex-x86_64-rootfs = final.runCommand "fex-rootfs" { nativeBuildInputs = [ final.erofs-utils ]; } ''
+      mkdir -p rootfs/run/opengl-driver
+      cp -R "${final.pkgsCross.gnu64.mesa-asahi-edge}"/* rootfs/run/opengl-driver/
+      mkfs.erofs $out rootfs/
+    '';
+  };
+
+  overlays = [
+    nixos-apple-silicon-overlay
+    overlay
+  ];
 in
-overlayed
-// {
-  virglrenderer = prev.virglrenderer.overrideAttrs (old: {
-    src = final.fetchurl {
-      url = "https://gitlab.freedesktop.org/asahi/virglrenderer/-/archive/asahi-20241205.2/virglrenderer-asahi-20241205.2.tar.bz2";
-      hash = "sha256-mESFaB//RThS5Uts8dCRExfxT5DQ+QQgTDWBoQppU7U=";
-    };
-    mesonFlags = old.mesonFlags ++ [ (final.lib.mesonOption "drm-renderers" "asahi-experimental") ];
-  });
-  libkrun = final.callPackage ./libkrun.nix { };
-  mesa-asahi-edge = final.callPackage ./mesa.nix { inherit (overlayed) mesa-asahi-edge; };
-  muvm = final.callPackage ./muvm.nix { mesa-x86_64-linux = final.pkgsCross.gnu64.mesa-asahi-edge; };
-  fex = final.callPackage ./fex.nix { };
-  fex-x86_64-rootfs = final.runCommand "fex-rootfs" { nativeBuildInputs = [ final.erofs-utils ]; } ''
-    mkdir -p rootfs/run/opengl-driver
-    cp -R "${final.pkgsCross.gnu64.mesa-asahi-edge}"/* rootfs/run/opengl-driver/
-    mkfs.erofs $out rootfs/
-  '';
-}
+final: # The final argument is shared between all overlays
+prev:
+prev.lib.foldl' (result: overlay: result // overlay final (prev // result)) {} overlays
